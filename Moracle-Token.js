16e61cd89e23849fa93d05d52a7d84da485c6765
Jackson Roberts <jacksonroberts25@gmail.com>
2018-01-05 17:43:37 -0700

    initial commit

---
+++ Moracle-Token.js
@@ -0,0 +1,171 @@
+// based on lotion-coin
+
+let secp256k1 = require('secp256k1')
+let { randomBytes } = require('crypto')
+let createHash = require('sha.js')
+let vstruct = require('varstruct')
+let axios = require('axios')
+
+let TxStruct = vstruct([
+    { name: 'amount', type: vstruct.UInt64BE },
+    { name: 'senderPubKey', type: vstruct.Buffer(33) },
+    { name: 'senderAddress', type: vstruct.Buffer(32) },
+    { name: 'receiverAddress', type: vstruct.Buffer(32) },
+    { name: 'nonce', type: vstruct.UInt32BE }
+])
+
+exports.handler = function (state, rawTx) {
+    let tx = deserializeTx(rawTx)
+    if (!verifyTx(tx)) {
+        console.log("invalid transaction");
+        return
+    }
+
+    let senderAddress = tx.senderAddress.toString('hex')
+    let receiverAddress = tx.receiverAddress.toString('hex')
+
+    let senderBalance = state.balances[senderAddress] || 0
+    let receiverBalance = state.balances[receiverAddress] || 0
+
+    if (senderAddress === receiverAddress) {
+        console.log("cannot send tokens to yourself");
+        return
+    }
+    if (!Number.isInteger(tx.amount)) {
+        console.log("amount is not integer");
+        return
+    }
+    if (tx.amount > senderBalance) {
+        console.log("amount is not greater than balance");
+        return
+    }
+    if (tx.nonce !== (state.nonces[senderAddress] || 0)) {
+        console.log("invalid nonce");
+        return
+    }
+    senderBalance -= tx.amount
+    receiverBalance += tx.amount
+
+    state.balances[senderAddress] = senderBalance
+    state.balances[receiverAddress] = receiverBalance
+    state.nonces[senderAddress] = (state.nonces[senderAddress] || 0) + 1
+}
+
+function hashTx(tx) {
+    let txBytes = TxStruct.encode({
+        amount: tx.amount,
+        senderPubKey: tx.senderPubKey,
+        senderAddress: tx.senderAddress,
+        nonce: tx.nonce,
+        receiverAddress: tx.receiverAddress
+    })
+    let txHash = createHash('sha256')
+        .update(txBytes)
+        .digest()
+
+    return txHash
+}
+
+function signTx(privKey, tx) {
+    let txHash = hashTx(tx)
+    let signedTx = Object.assign({}, tx)
+    let { signature } = secp256k1.sign(txHash, privKey)
+    signedTx.signature = signature
+
+    return signedTx
+}
+
+function verifyTx(tx) {
+    if (
+        deriveAddress(tx.senderPubKey).toString('hex') !==
+        tx.senderAddress.toString('hex')
+    ) {
+        return false
+    }
+    let txHash = hashTx(tx)
+    return secp256k1.verify(txHash, tx.signature, tx.senderPubKey)
+}
+
+function serializeTx(tx) {
+    let serializable = Object.assign({}, tx)
+    for (let key in tx) {
+        if (Buffer.isBuffer(tx[key])) {
+            serializable[key] = tx[key].toString('base64')
+        }
+    }
+    return serializable
+}
+
+function deserializeTx(tx) {
+    let deserialized = tx
+        ;[
+            'senderPubKey',
+            'senderAddress',
+            'receiverAddress',
+            'signature'
+        ].forEach(key => {
+            deserialized[key] = Buffer.from(deserialized[key], 'base64')
+        })
+
+    return deserialized
+}
+
+function deriveAddress(pubKey) {
+    return createHash('sha256')
+        .update(pubKey)
+        .digest()
+}
+
+exports.client = function (url = 'http://localhost:3000') {
+    let methods = {
+        generatePrivateKey: () => {
+            let privKey
+            do {
+                privKey = randomBytes(32)
+            } while (!secp256k1.privateKeyVerify(privKey))
+
+            return privKey
+        },
+        generatePublicKey: privKey => {
+            return secp256k1.publicKeyCreate(privKey)
+        },
+        generateAddress: pubKey => {
+            return deriveAddress(pubKey)
+        },
+        getBalance: async (address) => {
+            let state = await axios.get(url + '/state').then(res => res.data)
+            return state.balances[address] || 0
+        },
+        send: async (privKey, { address, amount }) => {
+            let senderPubKey = methods.generatePublicKey(privKey)
+            let senderAddress = methods.generateAddress(senderPubKey)
+
+            let currentState = await axios.get(url + '/state').then(res => res.data)
+
+            let nonce = currentState.nonces[senderAddress.toString('hex')] || 0
+
+            let receiverAddress
+            if (typeof address === 'string') {
+                receiverAddress = Buffer.from(address, 'hex')
+            } else {
+                receiverAddress = address
+            }
+            let tx = {
+                amount,
+                senderPubKey,
+                senderAddress,
+                receiverAddress,
+                nonce
+            }
+
+            let signedTx = signTx(privKey, tx)
+            let serializedTx = serializeTx(signedTx)
+            serializedTx['transactionType'] = 'balance-transfer';
+            console.log(JSON.stringify(serializedTx));
+            let result = await axios.post(url + '/txs', serializedTx)
+            return result.data
+        }
+    }
+
+    return methods
+}
---
+++ client2.js
@@ -0,0 +1,101 @@
+let token = require('./Moracle-Token');
+let lotion = require('lotion');
+var fs = require("fs");
+var shea = require("shea");
+const express = require('express');
+var axios = require("axios");
+
+
+// Lotion / Tendermint setup
+
+let client = token.client();
+
+if (!fs.existsSync('addresses2.json')) {
+  console.log("No key file found, generating Moracle address.");
+  var key = client.generatePrivateKey();
+  var pubkey = client.generatePublicKey(key);
+  var address = client.generateAddress(pubkey);
+
+  var addresses_object = [];
+  addresses_object.push({ 'privatekey': key.toString('hex'), 'publickey': pubkey.toString('hex'), 'address': address.toString('hex') });
+  fs.writeFileSync('addresses2.json', JSON.stringify(addresses_object));
+  console.log("Keys saved to addresses.json");
+} else {
+  console.log("Loading private keys from file.");
+  var address_file = fs.readFileSync('addresses2.json');
+  var addresses_list = JSON.parse(address_file);
+  var key = Buffer.from(addresses_list[0].privatekey, 'hex');
+  var pubkey = client.generatePublicKey(key);
+  var address = client.generateAddress(pubkey);
+}
+
+console.log("Address: " + address.toString("hex"));
+
+
+
+let app = lotion({
+  initialState: {
+    pendingRequests: [],
+    balances: {
+      '57162ecf4008ca8ba81ae09d2018015058a12dec12ee434b660be76be11f4f28': 2600000,
+    },
+    nonces: {}
+  },
+  createEmptyBlocks: false,
+  logTendermint: true,
+ 
+  peers: ['localhost:46658'],
+  devMode: true,
+});
+
+
+app.use(token.handler);
+
+app.listen(3001);
+
+
+
+const localApiApp = express();
+
+localApiApp.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var url = "http://localhost:3001";
+localApiApp.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client.html');
+});
+//passthrough since lotion.js's default server doesn't have proper CORS support
+localApiApp.get('/state', function (req, res) {
+  axios.get(url + '/state')
+    .then(function (_response) {
+      res.send(JSON.stringify(_response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+localApiApp.get('/txs', function (req, res) {
+  axios.get(url + '/txs')
+    .then(function (_response) {
+      res.send(JSON.stringify(_response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+localApiApp.get('/getaddress', function (req, res) {
+  res.send(address.toString('hex'));
+});
+localApiApp.get('/baltransfer', function (req, res) {
+  client.send(key, {
+    amount: parseInt(req.query.amount),
+    address: req.query.address,
+  });
+  res.send('transfer processed');
+});
+
+localApiApp.listen(4241, () => console.log('Go to localhost:4242'));
+
